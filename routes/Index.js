a50082b5577b939e0930c22e424363db856eb9bf
vincenssa <vincenssa@gmail.com>
2026-03-17 20:49:44 +0700

    Update Index.js

--- routes/Index.js
+++ routes/Index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
-
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-
+    app.use(cookieParser()); // tambahan
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header(
@@ -14,6 +14,5 @@ module.exports = (app) => {
         );
         next();
     });
-
     app.use(express.static(path.join(__dirname, '..', 'public')));
 };
